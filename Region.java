5be135121509c63014ffa4576ee629ecf8af9325
28md28 <md28@williams.edu>
2025-05-10 15:55:55 -0400

    Fully moved over region class, tested and fixed a bugs in constructors, subregionList

--- Region.java
+++ Region.java
@@ -10,8 +10,8 @@ class Region{
 	public Region SE;
 	public Region SW;
 	public Region NW;
-	public Region[] subRegionList = {NE, SE, SW, NW};
-	private Point coord;
+	public Region[] subregionList;
+	public Point coord;
 	// Whether or not region has subregions
 	// NOT whether or not there is a point assigned
 	private boolean isDivided; 
@@ -24,7 +24,7 @@ class Region{
 		this.isDivided = false;
 	}
 
-	public Region(int x1, int y1, int x2, int y2, Point point) {
+	public Region(int x1, int y1, int x2, int y2, Point coord) {
 		this.X1 = x1;
 		this.Y1 = y1;
 		this.X2 = x2;
@@ -37,7 +37,7 @@ class Region{
 	 *  Set cooridnate of point in terminal node
 	 * 	Return true if successfuly completed
 	 * 	Return false is unsuccessful
-	 * **/
+	 */
 	public void setPoint(Point coord) {
 		if (isDivided == false) {
 			this.coord = coord;
@@ -47,26 +47,22 @@ class Region{
 		}
 	}
 
+	/**
+	 * Get current stored point
+	 */
 	public Point getPoint() {
 		return coord;
 	}
 
-	/* 
-	 * Get list of subregions 
-	 * Used for iterating through regions easily
-	 */
-	public Region[] getSubRegionList() {
-		return subRegionList;
-	}
-
 	/**
-	 * Sets quadrants
+	 * Sets all quadrants and marks as divided
 	 */
 	public void setQuadrants(Region NE, Region SE, Region SW, Region NW) {
 		this.NE = NE;
 		this.SE = SE;
 		this.SW = SW;
 		this.NW = NW;
+		subregionList = new Region[]{NE, SE, SW, NW};
 		this.coord = null;
 		this.isDivided = true;
 	}
@@ -104,10 +100,52 @@ class Region{
 		return !isDivided() && !storesPoint();
 	}
 
+	/** 
+	 * Divide region into four quadrants
+	 *  Returns true if successful division
+	 *  Returns false if region is already partitioned
+	 */
+	public boolean subDivide() {
+		if (!isDivided()) {
+			int midX = (X1 + X2) / 2;
+			int midY = (Y1+ Y2) / 2;
+
+			// Add new quadrants to the current region
+			setQuadrants(new Region(midX, midY, X2, Y2), 
+						 new Region(midX, Y1, X2, midY), 
+						 new Region(X1, Y1, midX, midY), 
+						 new Region(X1, midY, midX, Y2));
+			return true;
+		}
+		return false;
+	}
+
 	public static void main(String[] args) {
 		Region test = new Region(0, 0, 100, 100, new Point(10,10));
 		System.out.println("isDivided test: " + test.isDivided() + " | Expected output: false");
-		System.out.println("isEmpty test: " + test.isEmpty() + " | Exepcted output: true");
+		System.out.println("isEmpty test: " + test.isEmpty() + " | Exepcted output: false");
+		System.out.println("storesPoint test: " + test.storesPoint() + " | Expected output: true");
+		System.out.println("getPoint test: " + test.getPoint() + " | Expected output: 10,10");
+		System.out.println("-----------------");
+		System.out.println("SUBDIVIDED REGION");
+		System.out.println("-----------------");
+		test.subDivide();
+		System.out.println("isDivided test: " + test.isDivided() + " | Expected output: true");
+		System.out.println("isEmpty test: " + test.isEmpty() + " | Exepcted output: false");
+		System.out.println("storesPoint test: " + test.storesPoint() + " | Expected output: false");
+		System.out.println("getPoint test: " + test.getPoint() + " | Expected output: 10,10");
+		System.out.println("containsLocation(10,10) test: " + test.containsLocation(new Point(10,10)) + " | expected output: true");
+		System.out.println("containsLocation(10,100) test: " + test.containsLocation(new Point(10,100)) + " | expected output: true");
+		System.out.println("containsLocation(10,101) test: " + test.containsLocation(new Point(10,101)) + " | expected output: false");
+		System.out.println(test.subregionList);
+
+		// for (Region region : test.subregionList) {
+		// 	System.out.println(region.X1);
+		// 	System.out.println(region.Y1);
+		// }
+
+		System.out.println(test.isDivided());
+
 	}
 
 }
