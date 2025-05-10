2d07ae60bdbe90cfe0a35a9771da2eca1019b10f
28md28 <md28@williams.edu>
2025-05-10 13:11:48 -0400

    Will revise ADT later

--- Graph.java
+++ Graph.java
@@ -1,11 +1,11 @@
 public interface Graph{
 	/* Inserts a new cooridnate pair */
-	public void insert(int x, int y);
+	public abstract void insert(int x, int y);
 
 	/* Gets number of points in a certain branch */
-	public int getPointNumber(int x, int y);
+	public abstract int getPointNumber(int x, int y);
 
-	/* Divide until each region only contains one point */
-	public void subDivide();
+	// /* Divide until each region only contains one point */
+	// public abstract void subDivide(Region region);
 
 }
